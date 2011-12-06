1fca08ca82012b070219fdd9e701c0b9635d8c65
John Le <john@crowdflower.com>
2011-12-06 15:29:31 -0800

    recursion is a little weird

--- src/koans/recursion.clj
+++ src/koans/recursion.clj
@@ -16,16 +16,29 @@
 ;; peek on a list peeks the first element
 ;;
 ;; pop and peek always access most efficient location (for vector or list)
+;;
 (defn recursive-reverse [coll]
   (if (empty? coll)
     coll
-    (cons (peek coll) (recursive-reverse (pop coll)))
-    ))
+    (cons (peek coll) (recursive-reverse (pop coll)))))
+
+;; this will work for either list or vector
+(defn recursive-reverse-any [coll]
+  (if (empty? coll)
+    coll
+    (concat (recursive-reverse-any (rest coll)) (list (first coll)))))
 
 (defn factorial [n]
   (if (= n 0)
     1
     (* n (factorial (dec n)))))
+
+(defn factorial' [n]
+  (loop [n n
+         acc 1]
+    (if (= n 0)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
@@ -43,6 +56,15 @@
   "Yet more difficult the more steps you take"
   (= '(5 4 3 2 1) (recursive-reverse [1 2 3 4 5]))
 
+  "Doesn't work on lists"
+  (= '(1 2 3 4 5) (recursive-reverse '(1 2 3 4 5)))
+
+  "Working on vectors"
+  (= '(5 4 3 2 1) (recursive-reverse-any [1 2 3 4 5]))
+
+  "Working on lists"
+  (= '(5 4 3 2 1) (recursive-reverse-any '(1 2 3 4 5)))
+
   "Simple things may appear simple."
   (= 1 (factorial 1))
 
@@ -55,8 +77,11 @@
   "And eventually you must think harder"
   (= 24 (factorial 4))
 
+  "Special works as normal"
+  (= 24 (factorial' 4))
+
   "You can even deal with very large numbers"
-  (< 1000000000000000000000000N (factorial 1000N))
+  (< 1000000000000000000000000N (factorial' 1000N))
 
   "But what happens when the machine limits you?"
-  (< 1000000000000000000000000N (factorial 10000N)))
+  (< 1000000000000000000000000N (factorial' 10000N)))
